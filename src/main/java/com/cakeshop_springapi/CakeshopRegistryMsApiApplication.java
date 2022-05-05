0926ed4fd70593cf868a0b0bb39fc36c4c8ec8e2
kishore <you@example.comgit>
2022-05-05 18:29:10 +0530

    modified src

--- src/main/java/com/cakeshop_springapi/CakeshopRegistryMsApiApplication.java
+++ src/main/java/com/cakeshop_springapi/CakeshopRegistryMsApiApplication.java
@@ -2,7 +2,8 @@ package com.cakeshop_springapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class CakeshopRegistryMsApiApplication {
 

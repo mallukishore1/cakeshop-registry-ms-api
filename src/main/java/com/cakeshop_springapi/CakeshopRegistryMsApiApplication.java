91705438d8aecd5a5965aaa0aa60ec6eeffd110c
kishore <you@example.comgit>
2022-05-05 16:58:53 +0530

    add src pom.xml

---
+++ src/main/java/com/cakeshop_springapi/CakeshopRegistryMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.cakeshop_springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeshopRegistryMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeshopRegistryMsApiApplication.class, args);
+	}
+
+}
